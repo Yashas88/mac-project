a86565fc1dfd2ac3661f9722dd9671c8213e7858
Yashas D B <yashasbasavaraj@gmail.com>
2022-05-11 11:36:20 +0530

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 const App = () => {
   return (
-    <div className="App">
+    <Router>
 
       <Header/>
       <main className='py-3'>
         <Container>
-           <h1>Welcome to Fashion World</h1> 
-           <HomeScreen/>   
+           <Route path = '/' component = {HomeScreen} exact/> 
+           <Route path = '/product/:id' component = {ProductScreen} />   
+
         </Container>
        </main>
        <Footer/>
-    </div>
+    </Router>
   );
 }
 
