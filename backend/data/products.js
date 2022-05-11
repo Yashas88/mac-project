b39bd73e1d3f27d3dcc43d52445fdc22abf60f74
Yashas D B <yashasbasavaraj@gmail.com>
2022-05-11 19:50:39 +0530

    backend json server created

---
+++ backend/data/products.js
@@ -0,0 +1,79 @@
+const products = [
+    {
+        _id : '1',
+        name : 'Women Black Ethnic Motifs Printed Kurta with Palazzos & With Dupatta',
+        image : '/images/kurts2.webp',
+        description : 
+            'Black printed Kurta with Palazzos with dupatta  Kurta design: Ethnic motifs printed',
+        brand : 'Libas',
+        category : 'Kurta Sets',
+        price : 2100,
+        rating : 4.0,
+        numReviews : 3
+    },
+
+    {
+        _id : '2',
+        name : 'Women Black Ethnic Motifs Printed Kurta with Palazzos & With Dupatta',
+        image : '/images/kurts2.webp',
+        description : 
+            'Black printed Kurta with Palazzos with dupatta  Kurta design: Ethnic motifs printed',
+        brand : 'Libas',
+        category : 'Kurta Sets',
+        price : 2100,
+        rating : 4.5,
+        numReviews : 2
+    },
+
+    {
+        _id : '3',
+        name : 'Women Black Ethnic Motifs Printed Kurta with Palazzos & With Dupatta',
+        image : '/images/kurts2.webp',
+        description : 
+            'Black printed Kurta with Palazzos with dupatta  Kurta design: Ethnic motifs printed',
+        brand : 'Libas',
+        category : 'Kurta Sets',
+        price : 2100,
+        rating : 3.5,
+        numReviews : 9
+    },
+    {
+        _id : '4',
+        name : 'Women Black Ethnic Motifs Printed Kurta with Palazzos & With Dupatta',
+        image : '/images/kurts2.webp',
+        description : 
+            'Black printed Kurta with Palazzos with dupatta  Kurta design: Ethnic motifs printed',
+        brand : 'Libas',
+        category : 'Kurta Sets',
+        price : 2100,
+        rating : 4,
+        numReviews : 5
+    },
+    {
+        _id : '5',
+        name : 'Women Black Ethnic Motifs Printed Kurta with Palazzos & With Dupatta',
+        image : '/images/kurts2.webp',
+        description : 
+            'Black printed Kurta with Palazzos with dupatta  Kurta design: Ethnic motifs printed',
+        brand : 'Libas',
+        category : 'Kurta Sets',
+        price : 2100,
+        rating : 4.5,
+        numReviews : 6
+    },
+    {
+        _id : '6',
+        name : 'Women Black Ethnic Motifs Printed Kurta with Palazzos & With Dupatta',
+        image : '/images/kurts2.webp',
+        description : 
+            'Black printed Kurta with Palazzos with dupatta  Kurta design: Ethnic motifs printed',
+        brand : 'Libas',
+        category : 'Kurta Sets',
+        price : 2100,
+        rating : 4.5,
+        numReviews : 8
+    },
+
+]
+
+module.exports = products
