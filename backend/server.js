d1b7af0e7fd38c2b81557417495d46d9869640f0
Yashas D B <yashasbasavaraj@gmail.com>
2022-05-14 22:11:40 +0530

    added redux file

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import  connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -9,11 +10,20 @@ connectDB()
 
 const app = express()
 
+app.use((req, res, next) => {
+    console.log(req.originalUrl);
+    next()
+})
+
 app.get('/',(req, res) => {
     res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5001
 
