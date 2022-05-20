439081ee6a42a4ae0e0396640e91179eb9757661
Yashas D B <yashasbasavaraj@gmail.com>
2022-05-20 15:28:41 +0530

    register backend added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,5 +25,58 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+// get user profile
+//route: GET/api/users/profile
+// Private access
 
-export {authUser}
+
+const getUserProfile = asyncHandler(async (req, res) => {
+      const user = await User.findById(req.user._id)
+
+      if(user) {
+          res.json({ 
+            _id: user.id,
+            name: user.name,
+            email : user.email ,
+            isAdmin : user.isAdmin,
+          })
+      } else {
+          res.status(404)
+          throw new Error('User not found')
+      }
+})
+
+
+//register new user, POST/api/users, public access
+
+const registerUser = asyncHandler(async (req, res) => {
+    const {name , email, password} =  req.body
+
+    const userExists = await User.findOne({email})
+   
+  if(userExists){
+      res.status(400)
+      throw new Error('User Already Exists')
+  }
+
+  const user = await User.create({
+      name ,
+      email,
+       password
+  })
+
+  if(user){
+      res.status(201).json({
+          _id : user._id, 
+          name : user.name, 
+          email : user.email,
+          isAdmin : user.isAdmin,
+          token : generateToken(user._id)
+      })
+  }else{
+      res.status(400)
+      throw new Error('User not found')
+  }
+})
+
+export {authUser ,registerUser,  getUserProfile}
